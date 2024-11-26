9f745319fa0c15dfb51895858c5ec9dd422f1a73
Michel <1058979021@qq.com>
2024-11-26 21:47:47 +0800

    init project

---
+++ highlight_springboot/src/test/java/com/wisely/highlight_springboot/HighlightSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisely.highlight_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlightSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
